e218a477b09ffc3fa3c6d5d4431a1cf569513e50
Your Name <you@example.com>
2020-02-19 13:48:00 +0900

    111

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movies from "./Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>count => {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}*/
+
+//https://yts-proxy.now.sh/list_movies.json?sort_by=rating
+
+class App extends React.Component {
+  state = {
+    isLoadings: true,
+    movies: []
+  };
+
+  getMovie = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+
+    this.setState({ movies, isLoadings: false });
+  };
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  render() {
+    const { isLoadings, movies } = this.state;
+    return (
+      <section className="movieWrap">
+        {isLoadings ? (
+          <div className="loading">
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movies
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                year={movie.year}
+                poster={movie.medium_cover_image}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
